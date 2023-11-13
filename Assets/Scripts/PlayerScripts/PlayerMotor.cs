591169d86aeb2f068da0be382813c03c1b1924f5
Shinmaruu <bdzhou@iu.edu>
2023-11-12 22:39:07 -0500

    Basically, replaced old game with new game
    Also, did nighttime, got old time look, got door to work

---
+++ Assets/Scripts/PlayerScripts/PlayerMotor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor: MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    public float speed = 2f;
+    private bool isGrounded;
+    public float gravity = -9.8f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+    }
+
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+
+        //gravity
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
